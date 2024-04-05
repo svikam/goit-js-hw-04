4f307eac7cf27d21e92c37e09d87e7c84e7f4b4f
svikam <svikam1@gmail.com>
2024-04-05 21:44:42 +0300

    hw-04 5.04.24

--- js/task-1.js
+++ js/task-1.js
@@ -1,9 +1,29 @@
 'use strict';
-function makeTransaction(quantity, pricePerDroid) {
-    const totalPrice = quantity * pricePerDroid;
-    return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+function isEnoughCapacity(products, containerSize) {
+    const productsArray = Object.values(products);
+    let totalProducts = 0;
+    for (let value of productsArray) {
+        totalProducts += value;
+    }
+    if (totalProducts <= containerSize) {
+        return `true`;
+    } else {
+        return `false`;
+    }
 }
 
-console.log(makeTransaction(5, 3000));
-console.log(makeTransaction(3, 1000));
-console.log(makeTransaction(10, 500));
+console.log(
+    isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+); // true
+
+console.log(
+    isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+); // false
+
+console.log(
+    isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
+); // true
+
+console.log(
+    isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
+); // false
--- js/task-2.js
+++ js/task-2.js
@@ -1,9 +1,44 @@
 'use strict';
-function getShippingMessage(country, price, deliveryFee) {
-    const totalPrice = price + deliveryFee;
-    return `Shipping to ${country} will cost ${totalPrice} credits`;
+function calcAverageCalories(days) {
+    const caloriesArray = [];
+    for (let day of days) {
+        caloriesArray.push(day.calories);
+    }
+    let totalCalories = 0;
+    for (let value of caloriesArray) {
+        totalCalories += value;
+    }
+    if (days.length > 0) {
+        return totalCalories / caloriesArray.length;
+    } else {
+        return `0`;
+    }
 }
 
-console.log(getShippingMessage("Australia", 120, 50)); 
-console.log(getShippingMessage("Germany", 80, 20)); 
-console.log(getShippingMessage("Sweden", 100, 20)); 
+console.log(
+    calcAverageCalories([
+    { day: "monday", calories: 3010 },
+    { day: "tuesday", calories: 3200 },
+    { day: "wednesday", calories: 3120 },
+    { day: "thursday", calories: 2900 },
+    { day: "friday", calories: 3450 },
+    { day: "saturday", calories: 3280 },
+    { day: "sunday", calories: 3300 }
+    ])
+); // 3180
+
+console.log(
+    calcAverageCalories([
+    { day: "monday", calories: 2040 },
+    { day: "tuesday", calories: 2270 },
+    { day: "wednesday", calories: 2420 },
+    { day: "thursday", calories: 1900 },
+    { day: "friday", calories: 2370 },
+    { day: "saturday", calories: 2280 },
+    { day: "sunday", calories: 2610 }
+    ])
+); // 2270
+
+console.log(
+    calcAverageCalories([])
+); // 0
